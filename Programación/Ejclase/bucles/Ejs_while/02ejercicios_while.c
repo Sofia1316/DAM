20b002c25b4c1dcf294b8375c46f96087263f766
Sofia1316 <sofiiaa1316@gmail.com>
2024-10-28 20:00:19 +0100

    while_intento

--- Programación/Ejclase/bucles/Ejs_while/02ejercicios_while.c
+++ Programación/Ejclase/bucles/Ejs_while/02ejercicios_while.c
@@ -2,26 +2,36 @@
 #include <stdlib.h>
 
 int main(){
-	int i = 0;
-	int resto2;
-	int resto3;
-	int resto4 = 0;
-	int divisor1 = 100;
-	int divisor2 = 10;
-	int divisor3 = 1;
-	int sol1;
-	int sol2;
-	int sol3;
+	int i;
+	int resto2, resto3, resto4, resto5;
+	int sol1, sol2, sol3, sol4;
+	int resultado = 0;
+	int divisor = 1000;
 
 	printf("Introduce un número para la suma de ellos:");
-	scanf("%d", i);
-	while(i>1){
-		printf("%d", i);
-		sol1 = i / divisor1;
-		resto2 = i % divisor1;
-		sol2 = sol1 
-		resto3 = resto2  divisor2;
+	scanf("%d", &i);
+	while(divisor > 1){	
+		sol1 = i / divisor; //1000
+		resto2 = i % divisor;
+
+		divisor = divisor / 10;
+
+		sol2 = resto2 / divisor; //100
+		resto3 = resto2 % divisor;
+
+		divisor = divisor / 10;
+
+		sol3 = resto3 / divisor; //10
+		resto4 = resto3 % divisor;
+
+		divisor = divisor / 10;
+
+		sol4 = resto4 / divisor; //1
+		resto5 = resto4 % divisor;
 		
+		resultado = sol1 + sol2 + sol3 + sol4;
+		printf("%d\n", resultado);
+
 	}
 	
 	return EXIT_SUCCESS;
