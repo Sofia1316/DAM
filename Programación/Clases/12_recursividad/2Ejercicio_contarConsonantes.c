7071ec9e0e27669745a7f0085b818bc43b57348a
Sofia <cfgs@Info1-Pc15>
2025-02-06 14:11:47 +0100

    recursivo

---
+++ Programación/Clases/12_recursividad/2Ejercicio_contarConsonantes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Implementa una función recursiva que cuente cuántas consonantes tiene una cadena 
+(por ejemplo, “Hola” tiene 2 consonantes: ‘H’ y ‘l’).
+*/
+int buscar(char * palabra, int posicion, int contador){
+	//caso base
+	if(palabra[posicion]=='\0'){ 
+		return contador;
+	}
+
+	// recursividad
+	if(palabra[posicion]!='a' && palabra[posicion]!='e' && palabra[posicion]!='i' && palabra[posicion]!='o' && palabra[posicion]!='u' && palabra[posicion]!='A' && palabra[posicion]!='E' && palabra[posicion]!='I' && palabra[posicion]!='O' && palabra[posicion]!='U'){
+		contador++;
+	}
+
+	int ret = buscar(palabra,posicion+1,contador);
+	return ret;
+}
+
+int main(){
+	char palabra[5] = "hola";
+	int contador = 0;
+	int posicion = 0;
+
+	int total = buscar(palabra,posicion,contador);
+
+	printf("Número de consonantes de hola: %d\n", total);
+
+return EXIT_SUCCESS;
+}
--- Programación/Clases/12_recursividad/Ejercicio1_buscarBinarios.c
+++ Programación/Clases/12_recursividad/Ejercicio1_buscarBinarios.c
@@ -6,30 +6,33 @@ el int array y el int buscar serán iguales mientras que el in y el fin se irán
 estos son posiciones del array. Por lo que, si quiero hacer la búsqueda binaria, debo buscar
 un número partiendo el array de mitad en mitad
 */
+// siempre hay que comprobar que todos los condicionales de la función tengan un return
 
 int busq_bin(int * array, int a_buscar, int in, int fin){
-	// caso base
-	if();
-
 	int mitad = (in+fin) / 2;
-	if(a_buscar == array[mitad]){
-		return 1; // porque lo ha encontrado
-	} else if(a_buscar < array[mitad]){
-		int ret = busq_bin(array,a_buscar,ini,fin);
-		return ret;
-	} else if(a_buscar > array[mitad]){
-		int ret2 = busq_bin(array,a_busca);
+
+	// caso base
+	if(array[mitad] == a_buscar){
+		return mitad; // devuelve la posición en la que está
+	}
+	if (ini > fin){
+		return -1; // no lo ha encontrado
 	}
 
 	// caso recursivo
+	if(a_buscar < array[mitad]){
+		int ret = busq_bin(array,a_buscar,ini,mitad);
+		return ret;
+	} else if(a_buscar > array[mitad]){
+		int ret2 = busq_bin(array,a_buscar,mitad+1,fin);
+		return ret2;
+	}
 	
 }
 
 int main(){
 	int array[10] = {-5, 1, 9, 10, 11, 12, 17, 120, 150, 1500};
-
 	int a_buscar = 120;
-
 	int encontrado = busq_bin(array, a_buscar, 0, 9);
 
 	return EXIT_SUCCESS;
