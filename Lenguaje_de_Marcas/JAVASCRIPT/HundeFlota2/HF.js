5c33a5f4dd6f13b7625fb3bd019844e48048a6a2
sofia <sofiiaa1316@gmail.com>
2025-05-07 10:14:20 +0200

    javascript hunde flota 2 a medias

---
+++ Lenguaje_de_Marcas/JAVASCRIPT/HundeFlota2/HF.js
@@ -0,0 +1,16 @@
+// En donde el índice es el número de cuadrados que tengo
+let barcos = [4,3,2,1];
+// TABLERO DEL USUARIO -> matriz que guarda los barcos
+let tableroUsuario;
+
+// Posición es la cadena que nos devuelve el id
+function esBarco2(posicion1, posicion2, m){
+    /*if(m[i][j] == m[][]){
+
+    }*/
+}
+
+function principal(){
+
+}
+window.addEventListener("DOMContentLoaded", principal, false);
